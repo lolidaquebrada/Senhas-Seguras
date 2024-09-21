b3120ca03bd7406522ef9aba31e67e9343de59ba
junin da quebrada <107425738+lolidaquebrada@users.noreply.github.com>
2024-09-21 19:45:02 -0300

    Senhas Seguras

--- javascript-gerador-senha/main.js
+++ javascript-gerador-senha/main.js
@@ -97,6 +97,5 @@ console.log(alfabeto);
             forcaSenha.classList.add('fraca');
         }
         const valorEntropia = document.querySelector('.entropia');
-valorEntropia.textContent = 2**Math.floor(entropia)/(100e6*60*60*24);
-
+        valorEntropia.textContent = "Um computador pode levar até " + Math.floor(2**entropia/(100e6*60*60*24)) + " dias para descobrir essa senha.";
     }
